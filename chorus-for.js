acfeb96e673471e4fbe768abe6129ea7cab350a1
Gloria <gloedem@gmail.com>
2021-06-24 13:01:09 -0400

    modifications...

--- chorus-for.js
+++ chorus-for.js
@@ -3,3 +3,17 @@ for (let repeat = 0; repeat < 10; repeat++) {
   console.log(chorus);
 }
 console.log("Until the sun comes up!")
+
+for (let i = 100; i <= 200; i++) {
+  if (i % 3 === 0) {
+    if (i % 4 === 0) {
+      console.log("LoopyLighthouse");
+    } else {
+      console.log("Loopy");
+    }
+  } else if (i % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(i)
+  }
+}
