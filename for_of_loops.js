5501a4f39b0542e3f047b9630699e84eac224ce4
Gloria <gloedem@gmail.com>
2021-07-02 00:53:58 -0400

    completed for..of loops

--- for_of_loops.js
+++ for_of_loops.js
@@ -1,13 +1,17 @@
-const amounts = [61.00, 52.25, 119.99, 5.00]
+const amounts = [60.0, 52.25, 119.99, 5.0];
 let total = 0;
-for (let i=0; i<amounts.length; i++) {
+for (let i = 0; i < amounts.length; i++) {
   total += amounts[i];
 }
-console.log('Order total is: ', total);
+console.log("Order total is: ", total);
 
 // For..of loop -> preferred method for looping over iterable objects (arrays)!
 let total2 = 0;
 for (let amount of amounts) {
   total2 += amount;
 }
-console.log(`Order total is: $${total2}`)
+console.log(`Order total is: $${total2}`);
+console.log(total2);
+console.log(total)
+
+
