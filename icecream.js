5ca09fcb40ff9f929c5e97080c75feeaee8e66ed
Gloria <gloedem@gmail.com>
2021-07-07 09:47:59 -0400

    exercises on array of ice cream flavours

---
+++ icecream.js
@@ -0,0 +1,17 @@
+const iceCreamFlavors = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavors);
+//Add 'root beer' to the array
+iceCreamFlavors.push("root beer");
+console.log(iceCreamFlavors);
+// log the first flavor of the array
+console.log(iceCreamFlavors[0]);
+// log the last flavor of the array
+console.log(iceCreamFlavors[5]);
+//log the total number of flavours in the array
+console.log(iceCreamFlavors.length);
